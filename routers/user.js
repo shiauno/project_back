c0a32a0a87589cfd5a61179c99601cc96e202f47
shiauno <cindy36908926@gmail.com>
2025-02-24 16:36:52 +0800

    add profile edit router

--- routers/user.js
+++ routers/user.js
@@ -1,13 +1,14 @@
 import { Router } from 'express'
 import * as user from '../controllers/user.js'
 import * as auth from '../middlewares/auth.js'
+import upload from '../middlewares/upload.js'
 
 const router = Router()
 
 router.post('/', user.create)
 router.post('/login', auth.login, user.login)
 router.get('/profile', auth.jwt, user.profile)
-// router.patch('/edit', auth.jwt, upload, user.edit)
+router.patch('/edit', auth.jwt, upload, user.edit)
 router.patch('/refresh', auth.jwt, user.refresh)
 router.delete('/logout', auth.jwt, user.logout)
 
